ce287ce203be22a68be6c01f222bdfe43c0b8384
ElleBerry <48695394+ElleBerry@users.noreply.github.com>
2019-04-25 10:40:54 -0500

    finished exercise

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -3,7 +3,19 @@ $(document).ready(function() {
     $('#button').click(function(e) {
         $('dd').toggleClass('invisible');
     });
+
+    $('#yellow').click(function() {
+        $('ul').each(function(e){
+            $(this).children().last().css('background-color', 'yellow');
+        })
+    });
+
+    $('h3').click(function(e) {
+        $(this).next().children().css('font-weight', 'bold');
+    });
+
+    $('li').click(function(e) {
+        $('ul').parent().children().first().css('color', 'blue');
+    });
 });
 
-
-$('li').last().css('background-color', '#FF0');
